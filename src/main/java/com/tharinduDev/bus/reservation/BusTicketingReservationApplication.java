bce5c008a0c6846ea3ce83a12944ce188ceeff3b
tharindumunaweera <tharindumunaweera94@gmail.com>
2025-11-15 09:50:22 +1000

    Initial commit: Setup Spring Boot project structure

---
+++ src/main/java/com/tharinduDev/bus/reservation/BusTicketingReservationApplication.java
@@ -0,0 +1,13 @@
+package com.tharinduDev.bus.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTicketingReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusTicketingReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tharinduDev/bus/reservation/BusTicketingReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tharinduDev.bus.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTicketingReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
