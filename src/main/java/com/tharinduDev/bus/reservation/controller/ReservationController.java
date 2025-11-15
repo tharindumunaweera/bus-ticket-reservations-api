76c3edabd9dc4a41fccf7471717eba0c349a63a8
tharindumunaweera <tharindumunaweera94@gmail.com>
2025-11-15 11:44:43 +1000

    define application architecture

---
+++ src/main/java/com/tharinduDev/bus/reservation/controller/ReservationController.java
@@ -0,0 +1,9 @@
+package com.tharinduDev.bus.reservation.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/reservations")
+public class ReservationController {
+}
---
+++ src/main/java/com/tharinduDev/bus/reservation/service/ReservationService.java
@@ -0,0 +1,8 @@
+package com.tharinduDev.bus.reservation.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReservationService {
+}
